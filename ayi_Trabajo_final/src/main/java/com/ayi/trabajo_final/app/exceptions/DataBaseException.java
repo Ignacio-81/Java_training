65b6976e8654bfba1d4424a4c10f7aeb380e0040
Ignacio.Badella <77839981+Ignacio-81@users.noreply.github.com>
2022-10-03 19:41:20 -0300

    clean

--- ayi_Trabajo_final/src/main/java/com/ayi/trabajo_final/app/exceptions/DataBaseException.java
+++
@@ -1,9 +0,0 @@
-package com.ayi.trabajo_final.app.exceptions;
-
-import java.sql.SQLException;
-
-public class DataBaseException extends SQLException {
-    public DataBaseException(String reason) {
-        super(reason);
-    }
-}
--- ayi_Trabajo_final/src/main/java/com/ayi/trabajo_final/app/exceptions/ReadAccessException.java
+++
@@ -1,8 +0,0 @@
-package com.ayi.trabajo_final.app.exceptions;
-
-public class ReadAccessException extends Exception{
-
-    public ReadAccessException(String message) {
-        super(message);
-    }
-}
